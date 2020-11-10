3420e9ed42ae483eb10a380707b3cdd7f0463529
Abel Valotta <abel.valotta@upa.edu.py>
2020-11-10 20:27:40 -0300

    Proyecto

---
+++ src/main/java/com/example/bitacora2/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.bitacora2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import static com.example.bitacora2.R.id.tvUsuario;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+
+}
